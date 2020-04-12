f2b2c8a59092ff340cc3dd56b6afdece6cbc5ecb
Nefi <nefisantanna@gmail.com>
2020-04-12 16:48:22 -0300

    Desenvolvendo com o Curso

---
+++ js/principal.js
@@ -0,0 +1,5 @@
+console.log("Foi carregador de um arquivo externo");
+
+var titulo = document.querySelector(".titulo"); <!-- document e o documento do site inteiro, e querySelector seleciona algo em especifico para manipular -->
+
+titulo.textContent = "Aparecida Nutricionista"; <!-- aqui pegamos o contexto do texto e alteramos -->
